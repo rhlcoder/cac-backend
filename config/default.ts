14a137a17186dd3087d39402be65c7c3d24e6433
rhl <rhlcoder@gmail.com>
2022-07-02 23:05:46 -0300

    feat: :wrench: config salt rounds for bcrypt

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,7 @@
 // Default configuration for the application.
 export default {
   port: 22016,
-  dbUri: 'mongodb://127.0.0.1:27017/shop'
+  dbUri: 'mongodb://127.0.0.1:27017/shop',
   // dbUri: 'mongodb://localhost:27017/shop' /* Mongo won't connect to 'localhost' 🤷‍♂️ */
+  saltWorkFactor: 10
 }
