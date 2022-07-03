41e1e35cb1940049521801b26242e09d654c0f47
rhl <rhlcoder@gmail.com>
2022-07-02 22:49:58 -0300

    fix: :bug: fixed database URI
    Mongo won't connect to 'localhost' 🤷‍♂️

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,6 @@
 // Default configuration for the application.
 export default {
   port: 22016,
-  dbUri: 'mongodb://localhost:27017/shop'
+  dbUri: 'mongodb://127.0.0.1:27017/shop'
+  // dbUri: 'mongodb://localhost:27017/shop' /* Mongo won't connect to 'localhost' 🤷‍♂️ */
 }
