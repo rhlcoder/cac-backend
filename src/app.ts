a20574bbb3148e8354cdd654c83d741d5ce27ad2
rhl <rhlcoder@gmail.com>
2022-07-02 22:28:14 -0300

    feat: :construction: basic functionality, pending dependencies

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import config from 'config'
+import connectDb from './utils/connect'
+import logger from './utils/logger'
+import routes from './routes'
+
+const app = express()
+const port = config.get<number>('port')
+
+const start = async (): Promise<void> => {
+  await connectDb()
+  app.listen(port, () => {
+    logger.info(`Server running on port: ${port}`)
+    routes(app)
+  })
+}
+
+void start()
